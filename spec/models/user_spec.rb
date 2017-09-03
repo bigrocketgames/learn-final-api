1af2e897dbf6a82c9cd1dd648cb24f96dfd882b0
Nathan Robbert <admin@bigrocketgames.com>
2017-09-03 13:01:52 -0700

    Passing user validation specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    
+    it 'requires an email and password upon creation' do
+      user = build(:user, email: nil, password: nil)
+
+      expect(user.valid?).to eq(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank", 
+        "Email is invalid"
+      ])
+    end
+    
+    it 'requires that an email is unique' do
+      create(:user)
+      user = build(:user)
+
+      expect(user.valid?).to eq(false)
+      expect(user.errors.full_messages).to eq([
+        "Email has already been taken"
+      ])
+    end
+    
+    it 'requires that an email is valid (contains an @ symbol and a (.com, .org, etc...)' do
+      user1 = build(:user, email: "test.com")
+      user2 = build(:user, email: "test@test")
+      user3 = build(:user, email: "test")
+
+      expect(user1.valid?).to eq(false)
+      expect(user2.valid?).to eq(false)
+      expect(user3.valid?).to eq(false)
+    end
+    
+  end
+    
+  describe 'on save' do
+
+    it 'hashes a password' do
+      user = build(:user)
+      user.save
+
+      expect(user.password_digest).not_to eq(user.password)
+    end
+
+  end
 end
