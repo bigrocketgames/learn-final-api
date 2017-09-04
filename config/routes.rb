c9c7b68acc78b8f7b9ec7220d89250e3a2e3f0d8
Nathan Robbert <admin@bigrocketgames.com>
2017-09-03 20:47:45 -0700

    Added /api/v1/auth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # /api/v1/users
-      resources :users, only: [:create, :show, :update, :destroy]
+      resources :users, only: [:create]
+
+      # /api/v1/auth
+      post '/auth', to: "auth#login"
+      post '/auth/refresh', to: "auth#refresh"
     end
   end
 end
