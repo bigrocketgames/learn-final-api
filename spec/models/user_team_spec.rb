7ec2c7495456790da0fe33654d7ba933aae47a96
Nathan Robbert <admin@bigrocketgames.com>
2017-09-03 18:44:44 -0700

    Passing relationships tests.

--- spec/models/user_team_spec.rb
+++ spec/models/user_team_spec.rb
@@ -3,15 +3,29 @@ require 'rails_helper'
 RSpec.describe UserTeam, type: :model do
   describe 'validations' do
     it 'requires a team_id and user_id' do
-      user_team = build(:user_team, fan_id: nil, team_id: nil)
+      user_team = build(:user_team, user_id: nil, team_id: nil)
 
       expect(user_team.valid?).to eq(false)
       expect(user_team.errors.full_messages).to eq([
-        "Fan must exist", 
+        "User must exist", 
         "Team must exist", 
-        "Fan can't be blank", 
+        "User can't be blank", 
         "Team can't be blank"
       ])
+    end
+  end
+
+  describe 'relationships' do
+    it 'belongs to a user' do
+      user_team = create(:user_team)
+
+      expect(user_team.user.id).to_not eq(nil)
+    end
+
+    it 'belongs to a team' do
+      user_team = create(:user_team)
+
+      expect(user_team.team.id).to_not eq(nil)
     end
   end
 end
