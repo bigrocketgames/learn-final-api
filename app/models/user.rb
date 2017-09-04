173978ab5b692e04e48ce40a73a54f1c69fa9ac0
Nathan Robbert <admin@bigrocketgames.com>
2017-09-03 20:47:28 -0700

    Added downcase_email to make email non-case sensitive for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   has_many :user_teams
   has_many :favorite_teams, through: :user_teams, source: :team
 
+  before_save :downcase_email
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  private
+
+  def downcase_email
+    self.email = self.email.delete(' ').downcase
+  end
+
 end
