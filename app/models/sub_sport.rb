4f7db9e3e2bc5721e44447e6a7bd96329b09135b
Nathan Robbert <admin@bigrocketgames.com>
2017-09-03 17:14:05 -0700

    Updated model relationships.

--- app/models/sub_sport.rb
+++ app/models/sub_sport.rb
@@ -1,6 +1,7 @@
 class SubSport < ApplicationRecord
   belongs_to :sport
+  has_many :teams
 
-  validates :name, presence: true, uniqueness: true
-  validates :sport_id, :conference_split, presence: true
+  validates :name, uniqueness: true
+  validates :name, :sport_id, :conference_split, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :user_teams
+  has_many :favorite_teams, through: :user_teams, source: :team
 
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
