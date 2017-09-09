50e12b9367270669f502ad61d09e428df46dbb09
Nathan Robbert <admin@bigrocketgames.com>
2017-09-09 11:50:31 -0700

    Added index and upgrade routes.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_token!, only: [:index]
+
+  def index
+    if @user.admin
+      @users = User.all
+      render json: @users
+    else
+      render json: {
+        errors: ["You are not authorized to view this content."]
+      }, status: 403
+    end
+  end
 
   def create
     @user = User.new(user_params)
@@ -9,6 +21,17 @@ class Api::V1::UsersController < ApplicationController
       render json: {
         errors: @user.errors
       }, status: 500
+    end
+  end
+
+  def upgrade
+    if @user.admin
+      @user = User.find_by(id: params[:id])
+      @user.upgrade
+    else
+      render json: {
+        errors: ["You are not authorized to upgrade this user."]
+      }, status: 403
     end
   end
 
