55479b52e1f9291384c3842b2de9a6022f7d20ab
Nathan Robbert <admin@bigrocketgames.com>
2018-02-13 17:44:54 -0800

    fix:  Adjusting relationship to prevent orphan issues with teams in a sub sport.

--- app/models/sub_sport.rb
+++ app/models/sub_sport.rb
@@ -1,6 +1,6 @@
 class SubSport < ApplicationRecord
   belongs_to :sport
-  has_many :teams
+  has_many :teams, dependent: :destroy
 
   validates :name, uniqueness: true
   validates :name, :sport_id, :conference_split, presence: true
