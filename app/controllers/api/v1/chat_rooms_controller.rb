258a7b1d77582b850d341ed7fbaf7c3c53774aba
bigrocketgames <admin@bigrocketgames.com>
2018-03-06 20:09:17 -0800

    fix:  Corrected ChatRoomsController name.

--- app/controllers/api/v1/chat_rooms_controller.rb
+++ app/controllers/api/v1/chat_rooms_controller.rb
@@ -1,9 +1,10 @@
-class API::V1::ChatRoomController < ApplicationController
+class Api::V1::ChatRoomsController < ApplicationController
 
   def show
     # get the chat room based on the chat room path
-    
+    @chatRoom = ChatRoom.find(params[:id])
     # render the json of the chat room through jbuilder
+    render 'chat_rooms/show.json.jbuilder', chatRoom: @chatRoom
   end
 
   def create
