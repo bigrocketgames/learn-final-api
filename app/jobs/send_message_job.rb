9963772026835f6b57e54022e347cec19a7dd0bb
bigrocketgames <admin@bigrocketgames.com>
2018-03-09 15:19:30 -0800

    feat:  Sending the message back to the front end via activejob and actioncable.

---
+++ app/jobs/send_message_job.rb
@@ -0,0 +1,9 @@
+class SendMessageJob < ApplicationJob
+  def perform(chatRoom, user, message)
+    MessagesChannel.broadcast_to chatRoom, message: build_message(user, message)
+  end
+
+  def build_message(user, message)
+    formattedMessage = {id: message.id, content: message.content, user: user.email, createdAt: message.created_at}
+  end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, :chat_room_id, :user_id, presence: true
+
+  after_commit { SendMessageJob.perform_later(self.chat_room, self.user, self) }
+
+  
 end
