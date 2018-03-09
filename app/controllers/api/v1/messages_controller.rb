867f18af486e7070bea47ac794f4d9f9c4359be3
bigrocketgames <admin@bigrocketgames.com>
2018-03-09 15:18:29 -0800

    fix:  Just initializing and then saving the new message.

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,25 +1,14 @@
-class API::V1::MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
 
   def create
     message = Message.new(message_params)
-    chatRoom = ChatRoom.find(message_params[:chat_room_id])
-
-    if message.save
-      @message = build_message(message)
-      ChatRoomChannel.broadcast_to chatRoom, @message
-    end
+    message.save
   end
 
   private
 
   def message_params
     params.require(:message).permit(:content, :chat_room_id, :user_id)
-  end
-
-  def build_message
-    Jbuilder.new do |message|
-      message.(self, :content, :user_id)
-    end
   end
 
 end
