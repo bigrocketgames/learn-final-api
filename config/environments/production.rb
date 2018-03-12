14601d2d4b5abc87ee0197e233f2591bbc99ba50
bigrocketgames <admin@bigrocketgames.com>
2018-03-11 20:25:57 -0700

    feat:  Upgrading to be ready for redis on Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://team-schedules-api.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http:////team-schedules.herokuapp.com', 'https:////team-schedules.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
