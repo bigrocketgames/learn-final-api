aa1e051b16240a6ec0b5a0b4c3c6f366e19c667c
Nathan Robbert <admin@bigrocketgames.com>
2018-07-26 17:28:32 -0700

    feat:  Adding season model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211173857) do
+ActiveRecord::Schema.define(version: 20180725053259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20180211173857) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "seasons", force: :cascade do |t|
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sports", id: :serial, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -61,6 +67,7 @@ ActiveRecord::Schema.define(version: 20180211173857) do
     t.integer "sub_sport_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes", default: 0
   end
 
   create_table "user_teams", id: :serial, force: :cascade do |t|
