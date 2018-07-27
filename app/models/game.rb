899be3ec00c4c282ce9c70faa4dd2c6c634acf7d
Nathan Robbert <admin@bigrocketgames.com>
2018-07-26 17:39:04 -0700

    feat:  Connecting season to gmes.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   has_one :chat_room, dependent: :destroy
+  belongs_to :season
   belongs_to :home_team, class_name: "Team"
   belongs_to :away_team, class_name: "Team"
 
