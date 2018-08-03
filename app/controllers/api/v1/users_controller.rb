7b89af38cf1d52cd3ef7d69d61bdbd7196079f07
Nathan Robbert <admin@bigrocketgames.com>
2018-08-02 22:02:04 -0700

    chore:  Add username to strong params.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -82,7 +82,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :admin)
+    params.require(:user).permit(:email, :password, :admin, :username)
   end
 
 end
