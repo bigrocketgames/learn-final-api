b43a9030e8b71675acb38b5311bedc2aeac56e63
Nathan Robbert <admin@bigrocketgames.com>
2018-08-02 19:23:12 -0700

    feat:  Messages return the username instead of email of the user creating the message.

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -6,6 +6,6 @@ class SendMessageJob < ApplicationJob
   end
 
   def build_message(user, message)
-    formattedMessage = {id: message.id, content: message.content, user: user.email, createdAt: message.created_at}
+    formattedMessage = {id: message.id, content: message.content, username: user.username, createdAt: message.created_at}
   end
 end
